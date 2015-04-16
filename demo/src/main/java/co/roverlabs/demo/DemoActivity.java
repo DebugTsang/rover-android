cc2146ea56e58d46fb4cba55174558cdedc35599
Sherry Yang <sherrystyang@gmail.com>
2015-04-16 11:03:51 -0400

    Added default notification sound/vibration/light

--- demo/src/main/java/co/roverlabs/demo/DemoActivity.java
+++ demo/src/main/java/co/roverlabs/demo/DemoActivity.java
@@ -32,13 +32,13 @@ public class DemoActivity extends ActionBarActivity {
         //mRover.setCustomerTraits(customerTraits);
 
         RoverCustomer roverCustomer = new RoverCustomer();
-        roverCustomer.setName("Sherry Yang");
-        roverCustomer.setEmail("sherry@roverlabs.co");
-        roverCustomer.addTraits("gender", "female");
+        roverCustomer.setName("Ata Namvari");
+        roverCustomer.setEmail("ata@roverlabs.co");
+        roverCustomer.addTraits("gender", "male");
 
         RoverConfigs roverConfigs = new RoverConfigs();
-        roverConfigs.setUuid("F352DB29-6A05-4EA2-A356-9BFAC2BB3316");
-        roverConfigs.setAppId("ff259b8f81ba2a2fd227445e2b3dbaca3e9552ff1663fa3f46e89a284bc9aaa0");
+        roverConfigs.setUuid("7931D3AA-299B-4A12-9FCC-D66F2C5D2462");
+        roverConfigs.setAppId("eae9edb6352b8fec6618d3d9cb96f2e795e1c2df1ad5388af807b05d8dfcd7d6");
         roverConfigs.setLaunchActivityName(this.getClass().getName());
         roverConfigs.setNotificationIconId(R.drawable.icon);
         roverConfigs.setSandBoxMode(false);
