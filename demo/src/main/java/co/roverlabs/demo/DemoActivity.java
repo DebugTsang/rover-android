71dc01a91a65addc19fdf6ddf774de705bb7b939
Sherry Yang <sherrystyang@gmail.com>
2015-04-20 17:20:10 -0400

    Cleaned up code

--- demo/src/main/java/co/roverlabs/demo/DemoActivity.java
+++ demo/src/main/java/co/roverlabs/demo/DemoActivity.java
@@ -11,7 +11,6 @@ import co.roverlabs.sdk.models.RoverCustomer;
 public class DemoActivity extends ActionBarActivity {
     
     public static final String TAG = DemoActivity.class.getName();
-    private Rover mRover;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,22 +18,12 @@ public class DemoActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo);
 
-        mRover = Rover.getInstance(this.getApplicationContext());
-
-        //Production
-        //UUID - "7931D3AA-299B-4A12-9FCC-D66F2C5D2462"
-        //App ID - "eae9edb6352b8fec6618d3d9cb96f2e795e1c2df1ad5388af807b05d8dfcd7d6"
-
-        //mRover.setCustomerName("Sherry Yang");
-        //mRover.setCustomerEmail("sherry@roverlabs.co");
-        //Map<String, Object> customerTraits = new HashMap<>();
-        //customerTraits.put("gender", "female");
-        //mRover.setCustomerTraits(customerTraits);
+        Rover rover = Rover.getInstance(this.getApplicationContext());
 
         RoverCustomer roverCustomer = new RoverCustomer();
-        roverCustomer.setName("Ata Namvari");
-        roverCustomer.setEmail("ata@roverlabs.co");
-        roverCustomer.addTraits("gender", "male");
+        roverCustomer.setName("Sherry Yang");
+        roverCustomer.setEmail("sherry@roverlabs.co");
+        roverCustomer.addTraits("gender", "female");
 
         RoverConfigs roverConfigs = new RoverConfigs();
         roverConfigs.setUuid("7931D3AA-299B-4A12-9FCC-D66F2C5D2462");
@@ -43,31 +32,9 @@ public class DemoActivity extends ActionBarActivity {
         roverConfigs.setNotificationIconId(R.drawable.icon);
         roverConfigs.setSandBoxMode(false);
 
-        mRover.setCustomer(roverCustomer);
-        mRover.setConfigurations(roverConfigs);
+        rover.setCustomer(roverCustomer);
+        rover.setConfigurations(roverConfigs);
 
-        mRover.startMonitoring();
+        rover.startMonitoring();
     }
-    
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_demo, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 }
