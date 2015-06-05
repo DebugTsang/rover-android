b60b3f42d6a28e22c2af4789a46fd3d586c68f5a
ata-n <ata-n@users.noreply.github.com>
2015-06-05 15:09:47 -0400

    Update VisitManager.java

--- rover/src/main/java/co/roverlabs/sdk/core/VisitManager.java
+++ rover/src/main/java/co/roverlabs/sdk/core/VisitManager.java
@@ -15,114 +15,45 @@ import co.roverlabs.sdk.model.Visit;
 /**
  * Created by ars on 15-06-02.
  */
-public class VisitManager {
-    static final String TAG = EstimoteHelper.class.getSimpleName();
+public class VisitManager implements RegionManager.IRegionListener {
+    static final String TAG = VisitManager.class.getSimpleName();
 
     public interface IVisitListener {
-        boolean shouldCreateVisit(Visit visit);
-        void onEnterLocation(Visit visit);
-        void onPotentiallyExitLocation(Location location);
-        void onExpireVisit(Visit visit);
-        void onEnterTouchpoint(List<Touchpoint> touchPoints);
-        void onExitTouchpoints(List<Touchpoint> touchPoints);
+        boolean shouldCreateVisit(VisitManager manager, Visit visit);
+        void onEnterLocation(VisitManager manager, Visit visit);
+        void onPotentiallyExitLocation(VisitManager manager, Location location, Visit visit);
+        void onExpireVisit(VisitManager manager, Visit visit);
+        void onEnterTouchpoint(VisitManager manager, List<Touchpoint> touchpoints, Visit visit);
+        void onExitTouchpoints(VisitManager manager, List<Touchpoint> touchpoints, Visit visit);
     }
 
-    final Context context;
+    private Visit mLatestVisit;
+    public IVisitListener mVisitListener;
+    private RegionManager mRegionManager;
 
-    private Visit mVisit;
-    private IVisitListener mVisitListener;
-
-    volatile boolean isLoggingEnabled;
-
-    IBeaconHelper beaconHelper;
     public VisitManager(Context context){
-        this.context = context;
-        beaconHelper = getDefaultBeaconHelper();
+        mRegionManager = new RegionManager(context);
+        mRegionManager.setListener(this);
+    }
 
+    public RegionManager getRegionManager() {
+        return this.mRegionManager;
     }
 
     public void setVisitListener(IVisitListener visitListener){
         this.mVisitListener = visitListener;
     }
-    public void removeVisitListener(){
-        this.mVisitListener = null;
+
+
+
+    @Override
+    public void onEnteredRegions(RegionManager manager, List<Region> regions){
+        // TO BE FILLED IN
     }
 
-    public void reStartMonitoring(String uuid){
-        stopMonitoring();
-        startMonitoring(uuid);
-    }
-
-    public void stopMonitoring(){
-        beaconHelper.stopMonitoring();
-    }
-    public void startMonitoring(String uuid){
-        beaconHelper.startMonitoring(uuid);
-    }
-
-
-    void enteredRegion(final Region region){
-
-        if(mVisit != null && mVisit.isInRegion(region) && mVisit.isAlive()) {
-            if(!mVisit.isInSubRegion(region)) {
-                onSubRegionEnter(region);
-            }
-            return;
-        }
-
-        Calendar now = Calendar.getInstance();
-        mVisit = new Visit();
-        //mVisit.setSimulation(mSandBoxMode);
-        //TODO: make customer statically accessible from Customer class
-        //mVisit.setCustomer((Customer)RoverUtils.readObjectFromSharedPrefs(mContext, RoverCustomer.class, null));
-        mVisit.setRegion(region);
-        mVisit.setTimeStamp(now.getTime());
-        mVisit.setLastBeaconDetectionTime(now);
-
-        if (mVisitListener != null){
-            mVisitListener.onEnterLocation(mVisit);
-        }
-
-        if (!mVisit.isInSubRegion(region)) {
-            onSubRegionEnter(region);
-        }
-    }
-
-    public void onSubRegionEnter(Region subRegion) {
-
-        List<Touchpoint> enteredTouchPoints = new ArrayList<>();
-        if(!mVisit.currentlyContainsWildCardTouchpoints()) {
-            for(Touchpoint wildCardTouchPoint : mVisit.getWildCardTouchpoints()) {
-                boolean doesContain = mVisit.getCurrentTouchpoints().contains(wildCardTouchPoint);
-                if (!doesContain){
-                    mVisit.addToCurrentTouchpoints(wildCardTouchPoint);
-                    enteredTouchPoints.add(wildCardTouchPoint);
-
-                }
-                if (isLoggingEnabled) {
-                    Log.d(TAG, "TouchPoint wild card (" + wildCardTouchPoint.getTitle() + ") - " + (doesContain ? "" : "not") + " seen before");
-                }
-            }
-        }
-
-        Touchpoint touchpoint = mVisit.getTouchpoint(subRegion);
-
-        if(touchpoint != null) {
-            boolean doesContain = mVisit.getCurrentTouchpoints().contains(touchpoint);
-            if (!doesContain){
-                mVisit.addToCurrentTouchpoints(touchpoint);
-                enteredTouchPoints.add(touchpoint);
-            }
-        }
-
-        if (mVisitListener != null) {
-            mVisitListener.onEnterTouchpoint(enteredTouchPoints);
-        }
-    }
-
-
-    public IBeaconHelper getDefaultBeaconHelper(){
-        return new EstimoteHelper(this);
+    @Override
+    public void onExitedRegions(RegionManager manager, List<Region> regions) {
+        // TO BE FILLED IN
     }
 
 }
